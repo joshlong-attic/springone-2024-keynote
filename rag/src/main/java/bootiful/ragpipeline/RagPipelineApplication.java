dd9e03ec44595aefa62d66d30f13f0d915ad3598
Josh Long <josh@joshlong.com>
2024-04-24 16:39:40 -0700

    gi

--- rag/src/main/java/bootiful/ragpipeline/RagPipelineApplication.java
+++ rag/src/main/java/bootiful/ragpipeline/RagPipelineApplication.java
@@ -9,9 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.Id;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -33,46 +36,96 @@ public class RagPipelineApplication {
     }
 
 
-    ApplicationRunner ragDemo(JdbcClient jdbcClient, VectorStore vectorStore) {
+    @Bean
+    ApplicationRunner ragDemo(ProductService productService, JdbcClient jdbcClient, VectorStore vectorStore,
+                              TokenTextSplitter tokenTextSplitter) {
         return args -> {
-
             var log = LoggerFactory.getLogger(getClass());
+//            initializeVectorDatabase(productService, jdbcClient, vectorStore, tokenTextSplitter);
 
-            jdbcClient.sql("delete from vector_store");
+            var product = productService.byId(7011);
+            log.info("searching for similar records to\n{}", product.id() + " " + product.name() + " " + product.description());
 
-            var products = jdbcClient
-                    .sql("SELECT * FROM products")
-                    .query((rs, rowNum) -> new Product(
-                            rs.getInt("id"),
-                            rs.getString("description"),
-                            rs.getString("name"),
-                            rs.getString("sku"),
-                            rs.getFloat("price")
-                    ))
-                    .list();
-
-
-            products
-                    .parallelStream()
-                    .forEach(product -> {
-
-                        var doc = new Document(
-                                product.description(),
-                                Map.of("price", product.price(),
-                                        "sku", product.sku(),
-                                        "name", product.name(),
-                                        "id", product.id()
-                                ));
-
-                        log.info("adding [" + product.id() + "] to the vector db.");
-                        vectorStore.accept(List.of(doc));
-
-                    });
-
+            log.info("results ");
+            var similar = productService.similarProductsTo(product);
+            log.info("found {}", similar.size());
+            for (var p : similar)
+                System.out.println(System.lineSeparator() + System.lineSeparator() +
+                        p.name() + System.lineSeparator() + p.id()+ System.lineSeparator() + p.description());
 
         };
     }
 
+    private void initializeVectorDatabase(ProductService productService,
+                                          JdbcClient jdbcClient, VectorStore vectorStore,
+                                          TokenTextSplitter tokenTextSplitter) {
+        var log = LoggerFactory.getLogger(getClass());
+        jdbcClient.sql("delete from vector_store");
+        var products = productService.products();
+        products
+                .parallelStream()
+                .forEach(product -> {
+                    var doc = new Document(
+                            product.name() + " " + product.description(),
+                            Map.of("price", product.price(),
+                                    "description", product.description(),
+                                    "sku", product.sku(),
+                                    "name", product.name(),
+                                    "id", product.id()
+                            )
+                    );
+                    log.info("adding [" + product.id() + "] to the vector db.");
+
+                    vectorStore.accept(List.of(doc));
+                });
+    }
+
+}
+
+@Service
+class ProductService {
+
+    private final JdbcClient jdbcClient;
+
+    private final VectorStore vectorStore;
+
+    private final RowMapper<Product> productRowMapper = (rs, rowNum) -> new Product(
+            rs.getInt("id"),
+            rs.getString("description"),
+            rs.getString("name"),
+            rs.getString("sku"),
+            rs.getFloat("price")
+    );
+
+    ProductService(JdbcClient jdbcClient, VectorStore vectorStore) {
+        this.jdbcClient = jdbcClient;
+        this.vectorStore = vectorStore;
+    }
+
+    Collection<Product> similarProductsTo(Product product) {
+        var similar = this.vectorStore.similaritySearch(product.description());
+        return similar
+                .parallelStream()
+                .map(doc -> (Integer) doc.getMetadata().get("id"))
+                .map(this::byId)
+                .toList();
+    }
+
+    Product byId(Integer id) {
+        return this.jdbcClient
+                .sql("select * from products where id = ? ")
+                .param(id)
+                .query(this.productRowMapper)
+                .single();
+    }
+
+    Collection<Product> products() {
+
+        return this.jdbcClient
+                .sql("SELECT * FROM products")
+                .query(this.productRowMapper)
+                .list();
+    }
 }
 
 record Product(@Id Integer id, String description, String name, String sku, float price) {
