584fbd75d4cc21a1aa289d8595d7c63f7e3127d2
Josh Long <josh@joshlong.com>
2024-04-25 17:32:18 -0700

    polish

--- rag/src/main/java/bootiful/ragpipeline/RagPipelineApplication.java
+++ rag/src/main/java/bootiful/ragpipeline/RagPipelineApplication.java
@@ -1,11 +1,12 @@
 package bootiful.ragpipeline;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.ChatClient;
-import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.SearchRequest;
@@ -26,12 +27,15 @@ import org.springframework.data.annotation.Id;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
 import javax.sql.DataSource;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static bootiful.ragpipeline.ProductsJsonLoaderJobConfiguration.JOB_NAME;
 
@@ -109,14 +113,19 @@ public class RagPipelineApplication {
             var product = productService.byId(40);
             log.info("searching for similar records to\n{}",  (product));
 
-            log.info("similar results:");
-            for (var p : productService.similarProductsTo( product))
-                System.out.println(p.toString());
+            var recommended = productService
+                    .recommend(product, "i want something that will look flattering and comfortable in the spring time.");
+
+            System.out.println(recommended.toString());
 
         };
     }
 
 }
+
+record Recommendation(Product product, String explanation) {
+}
+
 
 @Service
 class ProductService {
@@ -127,30 +136,63 @@ class ProductService {
 
     private final VectorStore vectorStore;
 
-    private final ChatClient singularity;
+    private final ObjectMapper objectMapper;
+    private final ChatClient ai;
 
     private final RowMapper<Product> productRowMapper = (rs, rowNum) -> new Product(
             rs.getInt("id"), rs.getString("description"),
             rs.getString("name"), rs.getString("sku"), rs.getFloat("price"));
 
-    ProductService(JdbcClient jdbcClient, VectorStore vectorStore, ChatClient singularity) {
+    ProductService(JdbcClient jdbcClient, VectorStore vectorStore, ObjectMapper objectMapper, ChatClient ai) {
         this.jdbcClient = jdbcClient;
         this.vectorStore = vectorStore;
-        this.singularity = singularity;
+        this.objectMapper = objectMapper;
+        this.ai = ai;
     }
 
-/*    Collection<Product> recommend(Product product, String query) {
+
+    Recommendation recommend(Product product, String query) throws Exception {
 
         var systemPrompt = """
                 
-                You are a shopping assistant whose job it is to help shoppers answer their questions and help them find the best alternative product.
-                
-
-                
+                You are a personal shopper assistant whose job it is to help a shopper answer 
+                questions and find the best product given the shopper's questions and the following choices:
+                                
+                {products}
+                                
+                the data is presented with three columns each, delimited by "|". The ID is the first column. Please return the ID of the product that would
+                be the single best response to their query. If you do not know, return an empty string. 
+                       
+                Here is their question:
+                                
+                {question}                              
+                                
+                If you do know, please return the response in a JSON structure with two attributes: one, `productId`, containing the ID of the product you have chosen,
+                and `explanation`, containing the reasons you think this is the best choice for the shopper.  
+                                
+                         
                 """;
-        var systemPromptTemplate = new SystemPromptTemplate()
 
-    }*/
+        var similar = similarProductsTo(product)
+                .stream()
+                .map(p -> p.id() + " | " + p.name() + " | " + p.description())
+                .collect(Collectors.joining(System.lineSeparator() + System.lineSeparator()));
+
+        var systemPromptTemplate = new PromptTemplate(systemPrompt);
+        var prompt = systemPromptTemplate.create(Map.of("products", similar, "question", query));
+
+        var response = this.ai.call(prompt).getResult().getOutput().getContent();
+        if (StringUtils.hasText(response)) {
+            var map = this.objectMapper
+                    .readValue(response, Map.class);
+            Assert.state(map.containsKey("productId"), "there must be an `productId` attribute");
+            Assert.state(map.containsKey("explanation"), "there must be an `explanations` attribute");
+            return new Recommendation(byId(Integer.parseInt((String) map.get("productId"))), (String) map.get("explanation"));
+        }
+
+        return null;
+
+    }
 
       Collection<Product> similarProductsTo(Product product) {
         var similar = this.vectorStore.similaritySearch(SearchRequest.query(product.name() + " " + product.description()));
