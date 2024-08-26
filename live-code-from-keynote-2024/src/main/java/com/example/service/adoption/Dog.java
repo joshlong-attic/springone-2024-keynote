7345b6ef9d6cea43d4a78b5ea1f5e7b9518937c9
Josh Long <josh@joshlong.com>
2024-08-26 10:18:11 -0700

    live

--- live-code-from-keynote-2024/src/main/java/com/example/service/adoption/Dog.java
+++
@@ -1,9 +0,0 @@
-package com.example.service.adoption;
-
-import org.springframework.data.annotation.Id;
-
-import java.time.LocalDate;
-
-record Dog(@Id Integer id, String name, String description, String owner,
-           LocalDate dob) {
-}
--- live-code-from-keynote-2024/src/main/java/com/example/service/adoption/DogRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.service.adoption;
-
-import org.springframework.data.repository.ListCrudRepository;
-
-interface DogRepository extends ListCrudRepository<Dog, Integer> {
-}
